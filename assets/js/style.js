0de6a63fe69d8fffdb90a5fd75c921948a2ede5c
coricathemer <coricathemer@gmail.com>
2021-01-16 12:22:28 -0700

    updated assets

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+// display current date at top of scheduler
+
+// display time blocks, rows, with standarded business hours
+
+// color code timeblocks for past, present, and future
+
+// onclick, add event to scheduler 
+
+// onclick, my event is saved in local storage
+
+// when I refresh my browser, my event is still there after page loads
