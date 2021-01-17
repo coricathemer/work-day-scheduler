6e615a554674e4dbeb3507fa490f35cf5aa4f3f9
coricathemer <coricathemer@gmail.com>
2021-01-17 12:36:55 -0700

    set timeblocks to color code based on time of day

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,13 +1,44 @@
-// display current date at top of scheduler
+// display current date at top of scheduler, this is working now and updates properly 
 var NowMoment = moment();
 var eDisplayMoment = document.getElementById('currentDay');
 eDisplayMoment.innerHTML = NowMoment.format('MM-DD-YYYY');
 console.log(NowMoment);
 
 // color code timeblocks for past, present, and future
+// moment().isAfter(Moment|String|Number|Date|Array, String);
+const rows = document.getElementsByClassName("col-md-10 description");
+let currentHour = parseInt(moment().format('H'));
 
-// onclick, add event to scheduler 
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
+    } else {
+      setColor(row, "white");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
+ 
 
 // onclick, my event is saved in local storage
+
+
+// load task from local storage
+
 
 // when I refresh my browser, my event is still there after page loads
